5ba91a4e3b3cd2f2d47a58127afca6a6bc0fc3b3
mohitsahu21 <mohitsahu1993@gmail.com>
2023-05-29 14:16:47 +0530

    password hashed done

--- Backend/routes/auth.js
+++ Backend/routes/auth.js
@@ -1,30 +1,35 @@
 const express = require('express');
 const router = express.Router();
 const mongoose = require('mongoose')
-const userModel = mongoose.model("User")
+const userModel = mongoose.model("User");
+const bcrypt = require('bcrypt')
 
-router.get('/',(req,res) =>{
-    res.send("home page")
-})
+
 router.post('/signup',(req,res)=>{
     const {name,email,password} = req.body;
     
-    
   
     userModel.findOne({email: email}).then((savedUser)=>{ 
+        if(!name || !email || !password){
+            return res.status(422).json({error: "Please fill all the fields"})
+        }
+
         if(savedUser){
             return res.status(422).json({error: "User already exist with the email"})
         }
-        const user = new userModel({
-            name,
-            email,
-            password
+        bcrypt.hash(password,12).then((hashedPass)=> {
+            const user = new userModel({
+                name,
+                email,
+                password: hashedPass
+            })
+            user.save().then((user)=>{
+                res.json({message : "saved successfully"})
+            }).catch((err)=>{
+                console.log(err)
+            })
         })
-        user.save().then((user)=>{
-            res.json({message : "saved successfully"})
-        }).catch((err)=>{
-            console.log(err)
-        })
+       
     }).catch((err)=>{
         console.log(err)
     })
