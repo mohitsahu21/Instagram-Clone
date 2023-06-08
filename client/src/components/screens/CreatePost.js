7bcbd058ac606830278ed87c12c4550622b84ace
mohitsahu21 <mohitsahu1993@gmail.com>
2023-06-08 19:37:31 +0530

    upload image to cloudnary done

--- client/src/components/screens/CreatePost.js
+++ client/src/components/screens/CreatePost.js
@@ -1,7 +1,57 @@
 
-import React from 'react';
+import React, { useState,useEffect } from 'react';
+import { Link, useNavigate } from 'react-router-dom';
+import M from "materialize-css"
+import axios from "axios";
 
-const CreatePost = ()=>{
+const CreatePost =  ()=>{
+    const navigate = useNavigate();
+    const [title,setTitle] = useState("");
+    const [body,setBody] = useState("");
+    const [image,setImage] = useState("");
+    const [url,setUrl] = useState("");
+    useEffect(()=>{
+        if(url){
+        fetch('/createpost',
+        {
+            method:"POST",
+            headers: {
+                "Content-Type":"application/json",
+                "Authorization": "Bearer "+ localStorage.getItem("jwt")
+            },
+            body: JSON.stringify({
+               title,
+               body,
+               pic: url
+            })  
+        }).then((res) => res.json())
+        .then((data) =>{
+           
+            if(data.error){
+                M.toast({html: data.error, classes: 'rounded red'})
+            }
+            else{
+                M.toast({html: "Post Created Successfully", classes: 'rounded green'});
+                navigate('/')
+
+            }
+        }).catch((err) => {console.log(err)})
+    }
+    },[url])
+
+
+    const postDetails= ()=>{
+        const data = new FormData()
+        data.append("file",image);
+        data.append("upload_preset","insta-clone");
+        data.append("cloud_name","dhryrs3lr");
+      axios.post("https://api.cloudinary.com/v1_1/dhryrs3lr/image/upload",data)
+        .then((res)=>{setUrl(res.data.secure_url)})
+        .catch(err=>console.log(err))
+        
+    }
+
+
     return (
         <div className='card' style={{
             margin: "30px auto",
@@ -9,18 +59,22 @@ const CreatePost = ()=>{
             padding: "20px",
             textAlign: "center"
         }}>
-            <input type='text' placeholder='title'/>
-            <input type='text' placeholder='body'/>
+            <input type='text' placeholder='title' value={title} 
+            onChange={(e)=>{setTitle(e.target.value)}}/>
+
+            <input type='text' placeholder='body' value={body} 
+            onChange={(e)=>{setBody(e.target.value)}} />
             <div className="file-field input-field">
       <div className="btn waves-effect waves-light #42a5f5 blue darken-1">
         <span>Upload Image</span>
-        <input type="file"/>
+        <input type="file" onChange={(e) => {setImage(e.target.files[0])}}/>
       </div>
       <div className="file-path-wrapper">
         <input className="file-path validate" type="text"/>
       </div>
     </div>
-    <button className="btn waves-effect waves-light #42a5f5 blue darken-1">Submit Post
+    <button className="btn waves-effect waves-light #42a5f5 blue darken-1"
+    onClick={postDetails}>Submit Post
     
     </button>
         </div>
